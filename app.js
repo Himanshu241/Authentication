8d2bad415e89805d4a7ca13c5be6c91c5bb33f17
himanshu <himanshuchaudhary429@gmail.com>
2023-01-27 20:11:35 +0530

    hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 const app = express()
 mongoose.set('strictQuery', true);
 app.use(express.static('public'))
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields:["password"]})
+
 const User = new mongoose.model("User",userSchema);
 app.get('/',(req,res)=>{
     res.render('home');
@@ -32,7 +32,7 @@ res.render('register')
 app.post("/register", (req,res)=>{
     const user = new User({
         email: req.body.username,
-        password: req.body.password 
+        password: md5(req.body.password)
     })
     user.save(function(err){
         if(!err){
@@ -46,7 +46,7 @@ app.post("/register", (req,res)=>{
 });
 app.post('/login',function(req,res){
     const email = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: email}, function(err,founduser){
         if(err){
             console.log(err)
